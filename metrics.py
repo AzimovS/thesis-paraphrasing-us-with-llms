451cbd35276f8f7dd7082266e59b436b1710e152
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-03-22 13:15:11 +0100

    add other metrics

--- metrics.py
+++ metrics.py
@@ -1,7 +1,6 @@
 import string
 import re
-from collections import Counter
-import math
+import textstat
 
 def total_characters(text):
     return len(text)
@@ -66,64 +65,29 @@ def number_of_urls(text):
     urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
     return len(urls)
 
-# def flesch_kincaid_grade_level(avg_syllables_per_word, avg_words_per_prop):
-#     return 0.39 * avg_words_per_prop + 11.8 * avg_syllables_per_word - 15.59
+def flesch_kincaid_grade_level(text):
+    return textstat.flesch_kincaid_grade(text)
 
-# def flesch_reading_ease(avg_syllables_per_word, avg_words_per_prop):
-#     return 206.835 - (84.6 * avg_syllables_per_word) - (1.015 * avg_words_per_prop)
+def flesch_reading_ease(text):
+    return textstat.flesch_reading_ease(text)
 
-# def dale_chall_readability(difficult_words_percentage, avg_prop_length):
-#     return 0.1579 * difficult_words_percentage + 0.0496 * avg_prop_length
+def dale_chall_readability(text):
+    return textstat.dale_chall_readability_score(text)
 
-# def automated_readability_index(total_words, total_characters, total_propositions):
-#     return 4.71 * (total_characters / total_words) + 0.5 * (total_words / total_propositions) - 21.43
+def automated_readability_index(text):
+    return textstat.automated_readability_index(text)
 
-# def coleman_liau_index(avg_prop_per_100_words, avg_letters_per_100_words):
-#     return 0.0588 * avg_letters_per_100_words - 0.296 * avg_prop_per_100_words - 15.8
+def coleman_liau_index(text):
+    return textstat.coleman_liau_index(text)
 
-# def gunning_fog(total_words, difficult_words, total_propositions):
-#     return 0.4 * ((total_words / total_propositions) + 100 * (difficult_words / total_words))
+def gunning_fog(text):
+    return textstat.gunning_fog(text)
 
-# def smog_index(difficult_words, total_propositions):
-#     return 1.0430 * math.sqrt((difficult_words * 30 / total_propositions)) + 3.1291
+def smog_index(text):
+    return textstat.smog_index(text)
 
-# def linsear_write_index(words_with_two_or_less_syllables, words_with_more_than_three_syllables, total_propositions):
-#     lw = (words_with_two_or_less_syllables + 3 * words_with_more_than_three_syllables) / total_propositions
-#     return lw / 2 if lw > 20 else (lw - 1) / 2
-
-# Example usage:
-# text = "Hello - my friend"
-# total_chars = total_characters(text)
-# upper_chars = uppercase_characters(text)
-# lower_chars = lowercase_characters(text)
-# special_chars = special_characters(text)
-# nums = numbers(text)
-# blanks_count = blanks(text)
-# words_count = number_of_words(text)
-# avg_word_length = average_length_of_words(text)
-# prop_count = number_of_propositions(text)
-# avg_prop_length = average_length_of_propositions(text)
-# punctuation_chars = punctuation_characters(text)
-# lowercase_words_count = lowercase_words(text)
-# uppercase_words_count = uppercase_words(text)
-# vocab_richness = vocabulary_richness(text)
-# urls_count = number_of_urls(text)
-
-# print("Total Characters:", total_chars)
-# print("Uppercase Characters:", upper_chars)
-# print("Lowercase Characters:", lower_chars)
-# print("Special Characters:", special_chars)
-# print("Numbers:", nums)
-# print("Blanks:", blanks_count)
-# print("Words:", words_count)
-# print("Average Word Length:", avg_word_length)
-# print("Propositions:", prop_count)
-# print("Average Proposition Length:", avg_prop_length)
-# print("Punctuation Characters:", punctuation_chars)
-# print("Lowercase Words:", lowercase_words_count)
-# print("Uppercase Words:", uppercase_words_count)
-# print("Vocabulary Richness:", vocab_richness)
-# print("URLs:", urls_count)
+def linsear_write_index(text):
+    return textstat.linsear_write_formula(text)
 
 import pandas as pd
 
@@ -145,19 +109,22 @@ metric_functions = [
     lowercase_words,
     uppercase_words,
     vocabulary_richness,
-    number_of_urls
+    number_of_urls,
+    flesch_kincaid_grade_level,
+    flesch_reading_ease,
+    dale_chall_readability,
+    automated_readability_index,
+    coleman_liau_index,
+    gunning_fog,
+    smog_index,
+    linsear_write_index
 ]
 
 if __name__ == '__main__':
-    print("HI")
     df = pd.read_excel(FILENAME, SHEETNAME)
-    df = df[:1]
-    sentence = df['User Story'][0]
-    sentence = "- - - - - -"
+    df = df[:2]
+    sentence = df['User Story'][1]
     print(sentence)
 
     for func in metric_functions:
-        # df['original_' + func.__name__] = df['User Story'].apply(func)
         print(f"{func.__name__}: {func(sentence)}")
-        # df['llm_' + func.__name__] = df['Paraphrased User Story'].apply(func)
-        # df['diff_' + func.__name__] = df['original_' + func.__name__] - df['llm_' + func.__name__]
